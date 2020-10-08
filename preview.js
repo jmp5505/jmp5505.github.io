07601e5a8fd0837995c92fbd310323e6c6740065
jmp5505 <1paultichy@gmail.com>
2020-10-08 18:57:29 +0200

    added shadows and animations

--- preview.js
+++ preview.js
@@ -1,9 +1,15 @@
+
 function show(x, y) {
     if (y == 1) {
         document.getElementById("show").innerHTML = "<img id=\"image\" src=\"" + x.toString() + ".png\">";
     }
-    document.getElementById(x.toString()).style = "background-color: rgb(209, 166, 240); color: #60228f";
-}
+    if (x != -1) {
+        document.getElementById(x.toString()).style = "background-color: rgb(209, 166, 240); color: #501a79;";
+    }
+    else {
+        document.getElementById("show").innerHTML = "<img id=\"image\" src=\"0.gif\">";
+    }    
+}    
 function unshow(x, y) {
     if (y == 1) {
         document.getElementById("show").innerHTML = "<img id=\"image\" src=\"" + "0" + ".png\">";
